e2c01d5402192f67a5f81cf0b8cf14138ae38e8e
Elton Andrade <eltonsa75@hotmail.com>
2019-11-01 16:39:24 -0300

    Atualizando Modelo de Negócio

--- Capitulo01/Capitulo01/Data/IESContext.cs
+++ Capitulo01/Capitulo01/Data/IESContext.cs
@@ -8,6 +8,8 @@ namespace Capitulo01.Data
 
         public DbSet<Departamento> Departamentos { get; set; }
         public DbSet<Instituicao> Instituicoes { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Disciplina> Disciplina { get; set; }
 
         public IESContext(DbContextOptions<IESContext> options) : base(options)
         {
@@ -25,6 +27,25 @@ namespace Capitulo01.Data
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=IESCasaDoCodigo;Trusted_Connection=True;MultipleActiveResultSets=true");
 
+        }
+
+        // Criando relacionamento via código usando Fluent API
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasKey(cd => new { cd.CursoID, cd.DisciplinaID });
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasOne(c => c.Curso)
+                .WithMany(cd => cd.CursosDisciplinas)
+                .HasForeignKey(c => c.CursoID);
+
+            modelBuilder.Entity<CursoDisciplina>()
+                .HasOne(d => d.Disciplina)
+                .WithMany(cd => cd.CursosDisciplinas)
+                .HasForeignKey(d => d.DisciplinaID);
         }
 
     }
---
+++ Capitulo01/Modelo/Cadastros/Curso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo.Cadastros
+{
+    class Curso
+    {
+
+        public long? CursoID { get; set; }
+        public string Nome { get; set; }
+
+        public long? DepartamentoID { get; set; }
+        public Departamento Departamento { get; set; }
+
+        public virtual ICollection<CursoDisciplina> CursosDisciplinas { get; set; }
+    }
+}
